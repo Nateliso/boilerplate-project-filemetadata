5dd735f1e505789c79c68580ffde349233d6a999
Nateliso <mhlanaliso18@gmail.com>
2025-03-03 09:42:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ require('dotenv').config()
 var app = express();
 
 app.use(cors());
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' }); // Files will be stored in "uploads/" folder
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
